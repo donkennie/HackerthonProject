583e9e7e4e5a46a5b82c251cd4b416225afd9086
donkennie <ajeigbekehinde160@gmail.com>
2022-10-12 04:14:30 +0100

    Add a request parameter folder to query the data

---
+++ HackerthonProject/RequestFeatures/PagedList.cs
@@ -0,0 +1,8 @@
+﻿namespace HackerthonProject.RequestFeatures
+{
+    public class PagedList<T> : List<T>
+    {
+
+        public MetaData MetaData { get; set; }
+    }
+}
---
+++ HackerthonProject/RequestFeatures/PagingParams.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerthonProject.RequestFeatures
+{
+    public abstract class PagingParams
+    {
+        const int maxPageSize = 50;
+
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+
+        public string? SearchTerm { get; set; }
+    }
+}
