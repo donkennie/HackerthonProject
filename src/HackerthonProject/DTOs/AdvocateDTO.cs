7c1140f5708e021102ba38abb4ee48981b548912
donkennie <ajeigbekehinde160@gmail.com>
2022-10-12 16:23:29 +0100

    Change Dto files to record class

--- src/HackerthonProject/DTOs/AdvocateDTO.cs
+++ src/HackerthonProject/DTOs/AdvocateDTO.cs
@@ -3,9 +3,9 @@
 namespace HackerthonProject.DTOs
 {
     public record AdvocateDTO(
-        string? Name, string? Short_bio, string? Long_bio, 
-        string? Profile_pic, int Advocate_years_exp, Company? Company, 
-        ICollection<Links>? Links
+        string Name, string Short_bio, string Long_bio, 
+        string Profile_pic, int Advocate_years_exp, Company Company, 
+        ICollection<string> Links
     );
     
 }
