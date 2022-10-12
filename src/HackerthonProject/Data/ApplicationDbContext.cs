076665711f8bcdeec34192d39010213ad3a59a03
donkennie <ajeigbekehinde160@gmail.com>
2022-10-12 16:22:10 +0100

    Implement seed data for each model

---
+++ src/HackerthonProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using HackerthonProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using System.Reflection;
+
+namespace HackerthonProject.Data
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContext) : base(dbContext)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+
+        public DbSet<Advocate> Advocates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ src/HackerthonProject/Data/Seed.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace HackerthonProject.Data
+{
+    public class Seed
+    {
+
+        public static async Task SeedAsync(ApplicationDbContext context, ILoggerFactory loggerFactory)
+        {
+			try
+			{
+				var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+				if (!context.Advocates.Any())
+				{
+					using var transaction = context.Database.BeginTransaction();	
+				}
+
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+        }
+    }
+}
